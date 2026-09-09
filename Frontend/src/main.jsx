2494ea4c63bb46d54b9a92ad7fca41d06113df58
lp7554499 <lp7554499@gmail.com>
2026-09-09 07:36:34 +0530

    fix: remove Redux Provider

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,12 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './app/index.css'
 import App from './app/App.jsx'
-import { store } from './app/app.store'
-import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
+  <StrictMode>
     <App />
-  </Provider>
+  </StrictMode>,
 )
